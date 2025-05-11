2748d5938cd8ada07ee6bcce0dfe8f5e781be568
tommyheo <jyhuh8775@naver.com>
2025-05-11 20:48:55 +0900

    requeest and response

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -4,8 +4,18 @@ const http = require("http");
 //서버 생성
 // createServer Callback function
 const server = http.createServer((req, res) => {
-    console.log(req);
-    process.exit();
+    //url, method, headers
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+
+    //response headers 설정
+    res.setHeader("Content-Type", "text/html");
+    //response html설정
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("</html>");
+    res.end();
 });
 
 // 서버가 종료되지 않고 계속 실행되어 요청을 듣게 하는 메서드
