86508c1f030f3c33a5b7b6fe8c713a5c080562e4
tommyheo <jyhuh8775@naver.com>
2025-05-11 21:13:37 +0900

    데이터 요청을 통해 파일작성

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -1,13 +1,43 @@
 //request를 받기 위해서 http 모듈 가져오기
 const http = require("http");
+const fs = require("fs");
 
 //서버 생성
 // createServer Callback function
 const server = http.createServer((req, res) => {
-    //url, method, headers
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+    //url 파싱
+    const url = req.url;
+    // method 파싱
+    const method = req.method;
 
+    if (url === "/") {
+        //response html설정
+        res.write("<html>");
+        res.write("<head><title>Enter Message</title></head>");
+        res.write(
+            "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+        );
+        res.write("</html>");
+        return res.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+        const body = [];
+        //특정 이벤트를 들을 수 있는 메서드 on()
+        req.on("data", (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on("end", () => {
+            parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            // console.log(parsedBody);
+            fs.writeFileSync("message.txt", message);
+        });
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+    }
     //response headers 설정
     res.setHeader("Content-Type", "text/html");
     //response html설정
