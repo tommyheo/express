daa32d41ea23b7535794b79686d3ac18ea22fa32
tommyheo <jyhuh8775@naver.com>
2025-05-11 21:26:07 +0900

    비동기의 이해

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -28,15 +28,16 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on("end", () => {
+        return req.on("end", () => {
             parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split("=")[1];
             // console.log(parsedBody);
-            fs.writeFileSync("message.txt", message);
+            fs.writeFile("message.txt", message, (err) => {
+                res.statusCode = 302;
+                res.setHeader("Location", "/");
+                return res.end();
+            });
         });
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
     }
     //response headers 설정
     res.setHeader("Content-Type", "text/html");
